e4bdf55603ee2f275d174ea153b5832ae7cac5a6
adityarajjhalak <aditya969072370@gmail.com>
2025-12-14 14:45:34 +0530

    add some components

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -2,17 +2,41 @@ import React from 'react'
 
 const Manager = () => {
   return (
- <div className=" mx-auto bg-slate-500 max-w-4xl">
-    <h1>Passop</h1>
-    <p>Your own password manager</p>
- <div className='text-white flex flex-col p-4  '>
-    <input className='bg-white rounded-4xl' type="" name='' id='' />
-    <div className="flex ">
-        <input className='bg-white' type="text" />
-        <input className='bg-white' type="text" />
+   <>
+<div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+
+
+ <div className="mycontainer">
+    <h1 className='text-3xl font-bold text-center'>
+ <span className='text-green-700'> &lt;</span>
+          Pass
+           <span className='text-green-700'>OP / &gt;</span>
+        
+
+    </h1>
+    <p className='text-green-900 text-center text-lg'>Your own Password Manager</p>
+ <div className='text-black flex flex-col p-4 gap-6'>
+    <input placeholder='Enter Website Url' className='bg-white rounded-full border border-green-600 w-full p-3 py-1' type="" name='' id='' />
+    <div className="flex gap-8 ">
+        <input placeholder='Enter Username' className="bg-white rounded-full border border-green-600 w-full p-3 py-1" type="text" />
+        <div className='relative'>
+        <input placeholder='Enter Password' className='bg-white rounded-full border border-green-600 w-full p-3 py-1' type="text" /> 
+        <span className='absolute right-0'>
+        show
+        </span>
+        </div>
     </div>
+    
+    <button className=' mx-auto flex justify-center items-center bg-green-500 w-fit px-6 py-2 rounded-full hover:bg-green-400 border-1 border-green-900'> 
+      <lord-icon
+    src="https://cdn.lordicon.com/efxgwrkc.json"
+    trigger="hover">
+</lord-icon> 
+      Add password</button>
  </div>
  </div>
+
+ </>
   )
 }
 
--- src/Components/Navabr.jsx
+++ src/Components/Navabr.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Navabr = () => {
   return (
-    <nav className='bg-purple-200 flex justify-between items-center px-4 h-15'>
-        <div className="logo font-bold  ">Passop</div>
+    <nav className=' bg-slate-900 text-amber-50'>
+      <div className='mycontainer  flex justify-between items-center px-4 py-2 h-15'> 
+
+        <div className="logo font-bold text-white text-2xl ">
+          <span className='text-green-700'> &lt;</span>
+          Pass
+           <span className='text-green-700'>OP / &gt;</span>
+          </div>
         <ul>
             <li className='flex gap-4'>
            <a className="hover:font-bold" href="/">Home</a>
@@ -11,6 +17,7 @@ const Navabr = () => {
            <a className="hover:font-bold" href="#">Contact</a>
            </li>
         </ul>
+        </div>
         </nav>
   )
 }
